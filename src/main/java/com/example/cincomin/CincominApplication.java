8ece6a46d85b3a7a6867637bdaccf12b5bbb022b
@sebas.davila.garcia <sebas.davila.garcia@gmail.com>
2021-04-23 19:16:55 -0500

    primer commit

---
+++ src/main/java/com/example/cincomin/CincominApplication.java
@@ -0,0 +1,13 @@
+package com.example.cincomin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CincominApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CincominApplication.class, args);
+    }
+
+}
